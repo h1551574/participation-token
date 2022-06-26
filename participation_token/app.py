a980583aa80969546da02c3fdbeea70a2416bd84
Florian Poreba <h1551574@s.wu.ac.at>
2022-06-26 15:03:22 -0700

    Batch Expiration and Activity Closing added!

--- participation_token/app.py
+++ participation_token/app.py
@@ -10,6 +10,7 @@ from sys import set_asyncgen_hooks
 
 from tempfile import mkdtemp
 import time
+from tokenize import Token
 from urllib import response
 from celery import Celery
 import celery
@@ -25,7 +26,7 @@ from pylti1p3.lineitem import LineItem
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+from sqlalchemy import false, func
 import qrcode
 from fpdf import FPDF
 import string
@@ -36,7 +37,7 @@ import os
 import sys
 import shutil
 
-from auth import is_activity_id_matching, is_admin_required
+from auth import is_admin_required
 
 
 class ReverseProxied(object):
@@ -67,11 +68,11 @@ config = {
     "SESSION_COOKIE_NAME": "flask-session-id",
     "SESSION_COOKIE_HTTPONLY": False,
     "SESSION_COOKIE_SECURE": True,   # should be True in case of HTTPS usage (production)
-    "SESSION_COOKIE_SAMESITE": None,  # should be 'None' in case of HTTPS usage (production)
+    "SESSION_COOKIE_SAMESITE": "None",  # should be 'None' in case of HTTPS usage (production)
     "DEBUG_TB_INTERCEPT_REDIRECTS": False,
     "SQLALCHEMY_DATABASE_URI": "sqlite:///databases/test_db.sqlite",
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-    "TOOL_URL": "https://wild-plums-clap-84-115-224-49.loca.lt",
+    "TOOL_URL": "https://793c-2a02-8388-3c1-bb00-62da-2606-b90c-fea0.eu.ngrok.io",
     "UPLOAD_FOLDER": "uploads",
 }
 app.config.from_mapping(config)
@@ -85,6 +86,8 @@ class Activity_config(db.Model):
     max_score = db.Column(db.Float, nullable=False)
     token_score = db.Column(db.Float, nullable=False)
     redirect_url = db.Column(db.String, nullable=False)
+    is_closed = db.Column(db.Boolean, nullable=False)
+
 
     def __repr__(self):
         return '<Id %r> ' % self.id + '<Max  %r> ' % self.max_score + '<TS  %r> ' % self.token_score
@@ -102,6 +105,8 @@ class Token_batch(db.Model):
     batch_id = db.Column(db.Integer, primary_key=True)
     activity_id = db.Column(db.Integer, primary_key=True)
     expired_by = db.Column(db.DateTime, nullable=False)
+    expired = db.Column(db.Boolean, nullable=False)
+
 
     def __repr__(self):
         return '<B_ID  %r> ' % self.batch_id + '<A_ID  %r> ' % self.activity_id + '<EXP_DATE  %r> ' % self.expired_by
@@ -129,7 +134,10 @@ celery = make_celery(app)
 @celery.task(bind=True)
 def generate_tokens_async(self,activity_url,activity_id, batch_id,amount_tokens,expired_by_date):
     tokens = []
-    expired_by_date = datetime.datetime.now()
+    # Deserialize expired_by_date since Celery automatically serializes
+    # data sent between Main Task and Workers into strings.
+    expired_by_date = datetime.datetime.strptime(expired_by_date, '%Y-%m-%dT%H:%M:%S.%f')
+    print(type(expired_by_date))
     x = range(amount_tokens)
     for n in x:
         token = secrets.token_urlsafe(nbytes=12)
@@ -143,7 +151,7 @@ def generate_tokens_async(self,activity_url,activity_id, batch_id,amount_tokens,
         self.update_state(state='PROGRESS',
                         meta={'current': n, 'total': amount_tokens,
                             'status': " of tokens generated!"})
-    token_batch = Token_batch(batch_id=batch_id,activity_id=activity_id,expired_by=expired_by_date)
+    token_batch = Token_batch(batch_id=batch_id,activity_id=activity_id,expired_by=expired_by_date,expired=False)
     db.session.add(token_batch)
     db.session.commit()
     filename = pdf_factory_async(self,tokens,activity_url, batch_id,activity_id)
@@ -156,11 +164,10 @@ def pdf_factory_async(task,tokens,redirect_url,batch_id,activity_id):
     load_token_url = []
     app.config['TOOL_URL']
     amount_tokens = len(tokens)
-    print(tokens)
     activity_id = str(activity_id)
+    print(redirect_url)
     for token in tokens:
-        print(token)
-        load_token_url.append([app.config['TOOL_URL']+'/load_token/?token='+str(token[0])+'&token_id='+str(token[1])+'&redirect_url='+redirect_url,token[0]])
+        load_token_url.append([app.config['TOOL_URL']+'/load_token/?token='+str(token[0])+'&batch_id='+str(batch_id)+'&token_id='+str(token[1])+'&redirect_url='+redirect_url,token[0]])
 
     class PDF(FPDF):
         def header(self):
@@ -268,16 +275,18 @@ def longtask():
     activity_id = session['activity_id']
 
     amount_tokens = int(request.form['num_tokens'])
- #   expiration_date_test = request.form['expiration_date']
- #   timezone_offset = request.form['timezone_offset']
+    days_until_expired = int(request.form['days_until_expired'])
 
-    expired_by_date = datetime.datetime.now()
-    max_batch_id = db.session.query(func.max(Grade_token.batch_id)).\
-    filter(Grade_token.activity_id == activity_id).\
+    expired_by_date = datetime.datetime.now() + datetime.timedelta(days=days_until_expired)
+    print("longtask")
+    print(type(expired_by_date))
+    max_batch_id = db.session.query(func.max(Token_batch.batch_id)).\
+    filter(Token_batch.activity_id == activity_id).\
     scalar()
 
+    # The query returns a tuple of size 1 therefore we need element 0 
     activity_url = str(db.session.query(Activity_config.redirect_url).\
-        filter(Activity_config.id == activity_id).first())
+        filter(Activity_config.id == activity_id).first()[0])
     if max_batch_id:
         max_batch_id += 1
     else:
@@ -323,6 +332,33 @@ def taskstatus(task_id):
     return jsonify(response)
 
 
+# Periodic Task (every 1h) to check if any token batches are expired
+@celery.task()
+def check_expired_batches():
+    now = datetime.datetime.now()
+    print("check expired:")
+    expired_batches = db.session.query(Token_batch).\
+        filter(Token_batch.expired == False).\
+        filter(Token_batch.expired_by < now).all()
+    for batch in expired_batches:
+        batch.expired = True
+        # Delete all tokens of expired batch
+        db.session.query(Grade_token).\
+        filter(Grade_token.activity_id == batch.activity_id).\
+        filter(Grade_token.batch_id == batch.batch_id).delete()
+    
+    db.session.commit()
+    print("check expired (END)")
+    return
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Expire Batches past expire by date every hour!
+    sender.add_periodic_task(datetime.timedelta(hours=1), check_expired_batches.s(), name='Checking expired batches')
+    # (Every Minute for debugging purposes!)
+    # sender.add_periodic_task(datetime.timedelta(minutes=1), check_expired_batches.s(), name='Checking expired batches')
+
+# LTI Setup
 PAGE_TITLE = 'Grade Token Example'
 
 
@@ -359,7 +395,7 @@ def get_jwk_from_public_key(key_name):
     return jwk
 
 
-
+# LTI Login Page
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
@@ -376,6 +412,9 @@ def login():
         .redirect(target_link_uri)
 
 
+# LTI Launch Page: Learning Platform intitiates launch
+# with a Post Requests which includes the necessary launch data (roles, activity_id, grade_service)
+# in the request header.
 @app.route('/launch/', methods=['POST', 'GET'])
 def launch():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
@@ -401,17 +440,61 @@ def launch():
 
     session['is_admin'] = user_is_admin
 
+    tpl_kwargs = {
+        'page_title': PAGE_TITLE,
+        'launch_data': message_launch.get_launch_data(),
+        'launch_id': message_launch.get_launch_id(),
+        'curr_user_name': message_launch_data.get('name', ''),
+        'curr_user_name': message_launch_data.get('name', '')
+    }
+    
+    curr_activity_id = activity_id
+
+    activity_config = Activity_config.query.filter_by(id=curr_activity_id).first()
+    tpl_kwargs['curr_activity_id'] = curr_activity_id
+
+
+    
+
+
+    if user_is_admin:
+        if activity_config is None:
+            return render_template('configure_activity.html', **tpl_kwargs)
+        
+        tpl_kwargs['curr_max_score'] = activity_config.max_score
+        tpl_kwargs['curr_token_score'] = activity_config.token_score
+        tpl_kwargs['curr_activity_url'] = activity_config.redirect_url
+        
+
+        batches = Token_batch.query.filter_by(activity_id=activity_id).all()
+        tpl_kwargs['curr_batches'] = batches
+        if activity_config.is_closed:
+            return render_template('activity_closed.html', **tpl_kwargs)
+        return render_template('manage_activity.html', **tpl_kwargs)
+    if activity_config.is_closed:
+        return render_template('activity_closed_student.html', **tpl_kwargs)
 
     # Loading in the Token that was previously set via QR Code Link
 
     token = session.get('token')
     token_id = session.get('token_id')
+    token_batch_id = session.get('token_batch_id')
+    token_batch = Token_batch.query.filter_by(batch_id=token_batch_id,activity_id=activity_id).first()
+    has_max_score = False
+
+    if token_batch is None:
+        return render_template('batch_expired_removed.html')
+    elif token_batch.expired:
+        return render_template('batch_expired_removed.html')
+    
     if token:
         print(token)
         has_token = True
-        grade_token = Grade_token.query.filter_by(id=token_id,activity_id=activity_id).first()
-        activity_config = Activity_config.query.filter_by(id=activity_id).first()
 
+        grade_token = Grade_token.query.filter_by(id=token_id,activity_id=activity_id).first()
+        
+        activity_config = Activity_config.query.filter_by(id=activity_id).first()
+        token_redeemed = grade_token.redeemed
         # Testing 
         token_ok = bcrypt.checkpw(token.encode('UTF-8'), grade_token.code)
         if token_ok and not grade_token.redeemed:
@@ -440,7 +523,6 @@ def launch():
             
             print("achieved_grade")
             print(achieved_grade)
-            has_max_score = False
             if achieved_grade < activity_config.max_score:
                 achieved_grade = achieved_grade+activity_config.token_score
             if achieved_grade >= activity_config.max_score:
@@ -467,38 +549,10 @@ def launch():
             db.session.commit()
     else:
         has_token = False
-
-    tpl_kwargs = {
-        'page_title': PAGE_TITLE,
-        'launch_data': message_launch.get_launch_data(),
-        'launch_id': message_launch.get_launch_id(),
-        'curr_user_name': message_launch_data.get('name', ''),
-        'curr_user_name': message_launch_data.get('name', ''),
-        'curr_token' : token,
-        'has_token' : has_token,
-    }
-    
-    curr_activity_id = activity_id
-
-    activity_config = Activity_config.query.filter_by(id=curr_activity_id).first()
-    tpl_kwargs['curr_activity_id'] = curr_activity_id
-
-
-    
-
-
-    if user_is_admin:
-        if activity_config is None:
-            return render_template('configure_activity.html', **tpl_kwargs)
-        
-        tpl_kwargs['curr_max_score'] = activity_config.max_score
-        tpl_kwargs['curr_token_score'] = activity_config.token_score
-        tpl_kwargs['curr_activity_url'] = activity_config.redirect_url
-        
-
-        batches = Token_batch.query.filter_by(activity_id=activity_id).all()
-        tpl_kwargs['curr_batches'] = batches
-        return render_template('manage_activity.html', **tpl_kwargs)
+    tpl_kwargs['curr_token'] = token
+    tpl_kwargs['has_token'] = has_token
+    tpl_kwargs['has_max_score'] = has_max_score
+    tpl_kwargs['token_redeemed'] = token_redeemed
     return render_template('redeem_token.html', **tpl_kwargs)
 
 
@@ -512,10 +566,9 @@ def get_jwks():
 
 @app.route('/configure_activity/', methods=['POST'])
 @is_admin_required
-@is_activity_id_matching
 def configure_activity():
     if request.method == 'POST':
-        activity_id = request.form['activity_id']
+        activity_id = session.get('activity_id')
         max_score = request.form['max_score']
         token_score = request.form['token_score']
         redirect_url = request.form['activity_url']
@@ -525,122 +578,50 @@ def configure_activity():
             activity_config.token_score = token_score
             activity_config.redirect_url = redirect_url
         else:
-            activity_config = Activity_config(id=activity_id, max_score=max_score, token_score=token_score, redirect_url=redirect_url)
+            activity_config = Activity_config(id=activity_id, max_score=max_score, token_score=token_score, redirect_url=redirect_url, is_closed = False)
             db.session.add(activity_config)
         db.session.commit()
         return render_template("activity_updated.html")
     return "Method Not Allowed", 403
 
-def generate_tokens_helper(batch_id,activity_id,amount_tokens,expired_by_date,retries = 0):
-    tokens = []
-    try:
-        x = range(amount_tokens)
-        for _ in x:
-            token = secrets.token_urlsafe(nbytes=12)
-            #code = token.encode('UTF-8')
-            code = bcrypt.hashpw(token.encode('UTF-8'), bcrypt.gensalt(14))
-            grade_token = Grade_token(code=code,batch_id=batch_id,activity_id=activity_id,redeemed=False)
-            db.session.add(grade_token)
-            db.session.flush()
-            #print(grade_token.id)
-            tokens.append([token,grade_token.id])
-        token_batch = Token_batch(batch_id=batch_id,activity_id=activity_id,expired_by=expired_by_date)
-        db.session.add(token_batch)
+@app.route('/close_activity/', methods=['POST'])
+@is_admin_required
+def close_activity():
+    if request.method == 'POST':
+        activity_id = session.get('activity_id')
+        Token_batch.query.filter_by(activity_id=activity_id).delete()
+        Grade_token.query.filter_by(activity_id=activity_id).delete()
+        activity_config= Activity_config.query.filter_by(id=activity_id).first()
+        activity_config.is_closed = True
         db.session.commit()
-    except sqlalchemy.exc.IntegrityError:
-        if retries < 5:
-            print("Oh No!")
-            generate_tokens_helper(retries=retries+1)
-        else:
-            return False
-    return tokens
+        return render_template("activity_closed.html")
+    return "Method Not Allowed", 403
 
+@app.route('/expire_batch/', methods=['POST'])
+@is_admin_required
+def expire_batch():
+    if request.method == 'POST':
+        activity_id = session.get('activity_id')
+        batch_id = request.form['batch_id']
+        expired_batch = Token_batch.query.filter_by(activity_id=activity_id,batch_id=batch_id).first()
+        expired_batch.expired = True
+        db.session.query(Grade_token).\
+        filter(Grade_token.activity_id == expired_batch.activity_id).\
+        filter(Grade_token.batch_id == expired_batch.batch_id).delete()
+        db.session.commit()
+        return render_template("activity_updated.html")
+    return "Method Not Allowed", 403
 
-
-def batch_pdf_factory(tokens,redirect_url,batch_id,activity_id):
-    load_token_url = []
-    app.config['TOOL_URL']
-
-    for token in tokens:
-        load_token_url.append([app.config['TOOL_URL']+'load_token/?token='+token[0]+'&token_id='+str(token[1])+'&redirect_url='+redirect_url,token[0]])
-
-    class PDF(FPDF):
-        def header(self):
-            # Logo
-            # Arial bold 15
-            self.set_font('Arial', 'B', 15)
-            # Move to the right
-            self.cell(10)
-            # Title
-            self.cell(170, 10, 'Course Title: LTI | Activity:'+ str(activity_id) + ' | Batch:'+ str(batch_id), 1, 0, 'C')
-            # Line break
-            self.ln(20)
-
-    pdf = PDF()
-    pdf.add_page()
-
-    pdf.set_font('Times', '', 9)
-    yoffset = 21
-    xoffsetRight = 155
-    xoffsetMiddle = 95
-    xoffsetLeft = 35
-    imagePosition = 0
-    imageSize = 30
-    yoffset_increment = 35
-    text_y_offset = 5.5
-    cell_width = 62
-    cell_height = 10
-
-    pdf.ln(16) # Top Offset
-
-    y_dash = 55
-    x1_dash = 0
-    x2_dash = 210
-
-    y1_hdash = 25
-    y2_hdash = 285
-    x_hdash_l = 75
-    x_hdash_r = 140
-
-    dir = activity_id
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-
-    for token in load_token_url:
-        qr_img = qrcode.make(token[0])
-        qr_img.save(activity_id+"/qr_code_"+activity_id+token[1]+".png")
-        if imagePosition==0:  
-            pdf.image(activity_id+"/qr_code_"+activity_id+token[1]+".png", xoffsetLeft, yoffset, imageSize)
-            pdf.cell(6,5)
-            pdf.cell(cell_width, cell_height, 'Code: '+token[1], 0, 0, 'C')
-            imagePosition=1
-        elif imagePosition==1:
-            pdf.image(activity_id+"/qr_code_"+activity_id+token[1]+".png", xoffsetMiddle, yoffset, imageSize)
-            pdf.cell(cell_width, cell_height, 'Code: '+token[1], 0, 0, 'C')
-            imagePosition=2
-        else:
-            pdf.image(activity_id+"/qr_code_"+activity_id+token[1]+".png", xoffsetRight, yoffset, imageSize)
-            pdf.cell(cell_width, cell_height, 'Code: '+token[1], 0, 0, 'C')
-            pdf.ln(imageSize+text_y_offset)
-            imagePosition=0
-            pdf.dashed_line(x1_dash, y_dash, x2_dash, y_dash, 1, 2)
-            pdf.dashed_line(x_hdash_l, y1_hdash, x_hdash_l, y2_hdash, 1, 2)
-            pdf.dashed_line(x_hdash_r, y1_hdash, x_hdash_r, y2_hdash, 1, 2)
-
-
-            y_dash +=yoffset_increment+0.4
-            yoffset += yoffset_increment+0.4
-        
-    dir = activity_id
-
-    try:
-        shutil.rmtree(dir)
-    except OSError as e:
-        print("Error: %s - %s." % (e.filename, e.strerror))
-    filename = 'b_'+str(batch_id)+'a_'+str(activity_id)+'.pdf'
-    filepath = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'],filename)
-    pdf.output(filepath, 'F')
-    return filename
+@app.route('/delete_batch/', methods=['POST'])
+@is_admin_required
+def delete_batch():
+    if request.method == 'POST':
+        activity_id = session.get('activity_id')
+        batch_id = request.form['batch_id']
+        Token_batch.query.filter_by(activity_id=activity_id,batch_id=batch_id).delete()
+        db.session.commit()
+        return render_template("activity_updated.html")
+    return "Method Not Allowed", 403
 
 @app.route('/uploads/<filename>', methods=['GET', 'POST'])
 @is_admin_required
@@ -655,11 +636,14 @@ def load_token():
     if request.method == 'GET':
         token = request.args.get('token')
         token_id = request.args.get('token_id')
+        token_batch_id = request.args.get('batch_id')
+
         redirect_url = request.args.get('redirect_url')
         if redirect_url:
             response = make_response(redirect(redirect_url))
             session['token'] = token
             session['token_id'] = token_id
+            session['token_batch_id'] = token_batch_id
             return response
     return('Something went wrong!')
 
--- participation_token/auth.py
+++ participation_token/auth.py
@@ -8,22 +8,10 @@ def is_admin_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if session.get('is_admin') is None:
+            print("No Admin Cookie!")
             return 'Unable to ckeck permissions! (Try to disable private mode)',403
 
         if not session['is_admin']:
-            return 'You are not authorized to view this page!',403
-        return view(**kwargs)
-    return wrapped_view
-
-# Wrapper that checks if the an admin user is not accessing content which is not related to his activity
-def is_activity_id_matching(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if session.get('activity_id') is None:
-            return 'Unable to ckeck permissions! (Try to disable private mode)',403
-        requested_activity = request.form['activity_id']
-
-        if requested_activity != session['activity_id']:
             return 'You are not authorized to view this page!',403
         return view(**kwargs)
     return wrapped_view
